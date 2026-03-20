8559588679d4e061ee3f68ea8cad0d94d5c20175
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-20 15:05:49 +0300

    run desktop

---
+++ desktop/src/main/kotlin/test/desktop/auth/App.kt
@@ -0,0 +1,18 @@
+package test.desktop.auth
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+internal object App {
+    init {
+        // todo
+    }
+}
+
+fun main() {
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            // todo
+        }
+    }
+}
