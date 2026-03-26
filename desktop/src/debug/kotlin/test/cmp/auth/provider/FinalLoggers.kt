3b4247b004b2c56cc238ee52ed3bac83664b6bd7
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-26 16:44:52 +0300

    loggers

--- desktop/src/debug/kotlin/test/cmp/auth/provider/FinalLoggers.kt
+++ desktop/src/debug/kotlin/test/cmp/auth/provider/FinalLoggers.kt
@@ -1,19 +1,37 @@
 package test.cmp.auth.provider
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 internal object FinalLoggers : Loggers {
+    private val dateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS", Locale.US)
+
     override fun create(tag: String): Logger {
-        return FinalLogger(tag = tag)
+        return FinalLogger(tag = tag, dateFormat = dateFormat)
     }
 }
 
 private class FinalLogger(
     private val tag: String,
+    private val dateFormat: DateFormat,
 ) : Logger {
     override fun debug(message: String) {
-        System.out.println("$tag: $message")
+        val text = """
+            ${dateFormat.format(Date())} $tag Debug
+            $message
+            
+        """.trimIndent()
+        System.out.println(text)
     }
 
     override fun warning(message: String) {
-        System.err.println("$tag: $message")
+        val text = """
+            ${dateFormat.format(Date())} $tag Warning
+            $message
+            
+        """.trimIndent()
+        System.err.println(text)
     }
 }
