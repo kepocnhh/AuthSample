e77245148b20191dd4dee275a3700b1d52b0285d
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-31 17:24:53 +0300

    wip

---
+++ shared/src/mock/kotlin/test/cmp/auth/Mocks.kt
@@ -0,0 +1,9 @@
+package test.cmp.auth
+
+import java.security.PrivateKey
+import test.cmp.auth.entity.EncryptedKey
+
+internal object Mocks {
+    val initialEncryptedKey: EncryptedKey? = null
+    val initialPrivateKey: PrivateKey? = null
+}
