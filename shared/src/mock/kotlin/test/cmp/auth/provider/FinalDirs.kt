47d3cc63f13dd59435b41a05b4f0a308380dc6bb
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-31 17:29:56 +0300

    mock

---
+++ shared/src/mock/kotlin/test/cmp/auth/provider/FinalDirs.kt
@@ -0,0 +1,10 @@
+package test.cmp.auth.provider
+
+import java.io.File
+
+internal class FinalDirs : Dirs {
+    override val files: File
+        get() = TODO("Dirs:files")
+    override val cache: File
+        get() = TODO("Dirs:cache")
+}
