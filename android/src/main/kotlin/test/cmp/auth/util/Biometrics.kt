b2ad97417931dc947e13acf16c916cc7cefa8ebf
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-04 15:03:19 +0300

    Biometrics

--- android/src/main/kotlin/test/cmp/auth/util/Biometrics.kt
+++ android/src/main/kotlin/test/cmp/auth/util/Biometrics.kt
@@ -6,29 +6,27 @@ import android.hardware.biometrics.BiometricPrompt
 import android.os.CancellationSignal
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import test.cmp.auth.BuildConfig
 import java.security.KeyStore
+import java.security.spec.AlgorithmParameterSpec
+import java.util.concurrent.atomic.AtomicReference
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import kotlin.coroutines.suspendCoroutine
 
-internal object Biometrics {
+internal class Biometrics(
+    private val context: Context,
+    private val coroutineScope: CoroutineScope,
+) {
     private val keyAlias = BuildConfig.APPLICATION_ID
-
-    private const val algorithm = KeyProperties.KEY_ALGORITHM_AES
-    private const val blocks = KeyProperties.BLOCK_MODE_GCM
-    private const val paddings = KeyProperties.ENCRYPTION_PADDING_NONE
-    private const val keyLength = 256
-    private const val authenticators = BiometricManager.Authenticators.BIOMETRIC_STRONG
-    private val callback = object : BiometricPrompt.AuthenticationCallback() {
-        override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-            TODO()
-        }
-
-        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-            TODO()
-        }
-    }
+    private val algorithm = KeyProperties.KEY_ALGORITHM_AES
+    private val blocks = KeyProperties.BLOCK_MODE_GCM
+    private val paddings = KeyProperties.ENCRYPTION_PADDING_NONE
+    private val keySize = 256
+    private val authenticators = BiometricManager.Authenticators.BIOMETRIC_STRONG
 
     private fun getKey(): SecretKey {
         val keyStore = KeyStore.getInstance("AndroidKeyStore")
@@ -39,7 +37,7 @@ internal object Biometrics {
             .Builder(keyAlias, purposes)
             .setBlockModes(blocks)
             .setEncryptionPaddings(paddings)
-            .setKeySize(keyLength)
+            .setKeySize(keySize)
             .setUserAuthenticationRequired(true)
             .setInvalidatedByBiometricEnrollment(true)
             .setUserAuthenticationParameters(0, KeyProperties.AUTH_BIOMETRIC_STRONG)
@@ -49,17 +47,50 @@ internal object Biometrics {
         return keyGenerator.generateKey()
     }
 
-    fun authenticate(
-        context: Context,
-        title: CharSequence,
-    ) {
-        val cipher = Cipher.getInstance("$algorithm/$blocks/$paddings")
-        cipher.init(Cipher.ENCRYPT_MODE, getKey())
-        BiometricPrompt.Builder(context)
-            .setTitle(title)
+    suspend fun BiometricPrompt.getCipher(issuer: Cipher): Cipher {
+        return coroutineScope.async {
+            suspendCoroutine { continuation ->
+                val callback = object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        continuation.resumeWith(Result.failure(IllegalStateException()))
+                    }
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        val result = runCatching {
+                            result.cryptoObject?.cipher ?: error("No cipher!")
+                        }
+                        continuation.resumeWith(result)
+                    }
+                }
+                authenticate(BiometricPrompt.CryptoObject(issuer), CancellationSignal(), context.mainExecutor, callback)
+            }
+        }.await()
+    }
+
+    suspend inline fun <reified T : AlgorithmParameterSpec> encrypt(decrypted: ByteArray, specs: AtomicReference<T>): ByteArray {
+        val issuer = Cipher.getInstance("$algorithm/$blocks/$paddings")
+        issuer.init(Cipher.ENCRYPT_MODE, getKey())
+        val cipher = BiometricPrompt.Builder(context)
+            .setTitle("encrypt")
             .setAllowedAuthenticators(authenticators)
             .setConfirmationRequired(true)
             .build()
-            .authenticate(BiometricPrompt.CryptoObject(cipher), CancellationSignal(), context.mainExecutor, callback)
+            .getCipher(issuer = issuer)
+        val encrypted = cipher.doFinal(decrypted)
+        val spec = cipher.parameters.getParameterSpec(T::class.java)
+        specs.set(spec)
+        return encrypted
+    }
+
+    suspend fun decrypt(encrypted: ByteArray, spec: AlgorithmParameterSpec): ByteArray {
+        val issuer = Cipher.getInstance("$algorithm/$blocks/$paddings")
+        issuer.init(Cipher.DECRYPT_MODE, getKey(), spec)
+        return BiometricPrompt.Builder(context)
+            .setTitle("encrypt")
+            .setAllowedAuthenticators(authenticators)
+            .setConfirmationRequired(true)
+            .build()
+            .getCipher(issuer = issuer)
+            .doFinal(encrypted)
     }
 }
