e99068020ff6e1b271bf259f2be036f2c1abcc7b
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-04-04 15:05:51 +0300

    Providers

---
+++ desktop/src/main/kotlin/test/cmp/auth/provider/Providers.kt
@@ -0,0 +1,11 @@
+package test.cmp.auth.provider
+
+internal class Providers(
+    val contexts: Contexts,
+    val dirs: Dirs,
+    val locals: Locals,
+    val loggers: Loggers,
+    val secrets: Secrets,
+    val hashes: Hashes,
+    val transformers: Transformers,
+)
